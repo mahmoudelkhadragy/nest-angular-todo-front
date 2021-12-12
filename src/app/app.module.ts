6bd63f418b98d09bf6afca535d19b583a69bf974
mahmoudelkhadragy <mahmoudelkhadragy38@gmail.com>
2021-12-12 03:20:47 +0200

    fix todo shape

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+// components
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
 import { TodoComponent } from './todo/todo.component';
+// angular material modules
 import { ToastrModule } from 'ngx-toastr';
 import { MatSelectModule } from '@angular/material/select';
 import { MatIconModule } from '@angular/material/icon';
@@ -18,6 +20,9 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { FormsModule } from '@angular/forms';
 import { MatCardModule } from '@angular/material/card';
+import { MatExpansionModule } from '@angular/material/expansion';
+import { MatMenuModule } from '@angular/material/menu';
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -39,6 +44,8 @@ import { MatCardModule } from '@angular/material/card';
     MatInputModule,
     MatCardModule,
     FormsModule,
+    MatExpansionModule,
+    MatMenuModule,
     ToastrModule.forRoot({
       closeButton: true,
       progressBar: true,

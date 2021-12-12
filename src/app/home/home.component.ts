e64c2707ce30f78e0a13f4364ef8335c9a75725a
mahmoudelkhadragy <mahmoudelkhadragy38@gmail.com>
2021-12-12 03:21:04 +0200

    add filter

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,15 +1,30 @@
 import { Component, OnInit } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { MatSelectChange } from '@angular/material/select';
+import { ApiService } from '../services/api.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent implements OnInit {
+  todos: any[] = [];
+  filteredTodos: any[] = [
+    { title: 'test', description: 'test description', status: 'OPEN' },
+    { title: 'develop', description: 'develop description', status: 'OPEN' },
+  ];
+  constructor(private apiService: ApiService, private dialog: MatDialog) {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  filterChanged(e: MatSelectChange) {
+    const value = e.value;
+    this.filteredTodos = this.filteredTodos;
   }
 
+  deleteTodo(e: Event) {
+    e.stopPropagation();
+    console.log('ffff');
+  }
 }

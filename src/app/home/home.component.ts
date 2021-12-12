d9d5cfb16cf2efef7b7bb9844cd331508271911f
mahmoudelkhadragy <mahmoudelkhadragy38@gmail.com>
2021-12-13 00:52:40 +0200

    make status methods

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { MatSelectChange } from '@angular/material/select';
+import { Todo } from '../models/todo.model';
 import { ApiService } from '../services/api.service';
 
 @Component({
@@ -9,7 +10,7 @@ import { ApiService } from '../services/api.service';
   styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent implements OnInit {
-  todos: any[] = [];
+  todos: Todo[] = [];
   filteredTodos: any[] = [
     { title: 'test', description: 'test description', status: 'OPEN' },
     { title: 'develop', description: 'develop description', status: 'OPEN' },
@@ -21,10 +22,21 @@ export class HomeComponent implements OnInit {
   filterChanged(e: MatSelectChange) {
     const value = e.value;
     this.filteredTodos = this.filteredTodos;
+    if (value) {
+      this.filteredTodos = this.filteredTodos.filter(
+        (todo: Todo) => todo.status === value
+      );
+    } else {
+      this.filteredTodos = this.todos;
+    }
+  }
+
+  statusChanged(value: string, todoId: number, i: number) {
+    console.log(value, todoId, i);
+    console.log(typeof value, typeof todoId, typeof i);
   }
 
   deleteTodo(e: Event) {
     e.stopPropagation();
-    console.log('ffff');
   }
 }

42279672162fa6729986331da6f7b882c087c74e
mahmoudelkhadragy <mahmoudelkhadragy38@gmail.com>
2021-12-13 23:54:02 +0200

    add register

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,15 +1,22 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { ApiService } from '../services/api.service';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
-  styleUrls: ['./register.component.scss']
+  styleUrls: ['./register.component.scss'],
 })
 export class RegisterComponent implements OnInit {
+  constructor(private apiService: ApiService) {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  register(registerForm: NgForm) {
+    if (registerForm.invalid) {
+      return;
+    }
+    const { username, password } = registerForm.value;
+    // this.apiService.
   }
-
 }

6b4f4bb2ec54574b54c109c7514b57fb13968fca
mahmoudelkhadragy <mahmoudelkhadragy38@gmail.com>
2021-12-13 23:55:07 +0200

    add logout and bind username in app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { ApiService } from './services/api.service';
+import { Component, OnInit } from '@angular/core';
+import jwtDecode from 'jwt-decode';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   isLogedIn = false;
+  username = '';
+  constructor(private apiService: ApiService) {}
+
+  ngOnInit(): void {
+    this.apiService.jwtUserToken.subscribe((token) => {
+      if (token) {
+        const decoded: any = jwtDecode(token);
+        this.username = decoded.username;
+      }
+      if (this.username) {
+        this.isLogedIn = true;
+      } else {
+        this.isLogedIn = false;
+      }
+    });
+  }
+
+  // logout method
+  logout() {
+    this.username = '';
+    this.apiService.logout();
+  }
 }

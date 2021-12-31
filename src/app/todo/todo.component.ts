dbb5d20b6e4030b1ec67a3b3fd4d190ebb03f958
mahmoudelkhadragy <mahmoudelkhadragy38@gmail.com>
2021-12-31 18:19:23 +0200

    create todo

--- src/app/todo/todo.component.ts
+++ src/app/todo/todo.component.ts
@@ -1,15 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Inject, OnInit } from '@angular/core';
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-todo',
   templateUrl: './todo.component.html',
-  styleUrls: ['./todo.component.scss']
+  styleUrls: ['./todo.component.scss'],
 })
 export class TodoComponent implements OnInit {
+  title = '';
+  description = '';
 
-  constructor() { }
+  constructor(
+    public dialogRef: MatDialogRef<TodoComponent>,
+    @Inject(MAT_DIALOG_DATA) public data: any
+  ) {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  onCancel() {
+    this.dialogRef.close();
   }
-
+  create() {
+    this.dialogRef.close({ title: this.title, description: this.description });
+  }
 }
